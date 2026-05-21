a980f503e5ee526d6469edad55b96f9d315095ad
Link4Electronics <link4electronics@gmail.com>
2026-05-21 03:16:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,29 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q asunder | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/pixmaps/asunder.svg
+export DESKTOP=/usr/share/applications/asunder.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/asunder \
+  /usr/bin/cdparanoia \
+  /usr/bin/fdkaac \
+  /usr/bin/flac \
+  /usr/bin/mac \
+  /usr/bin/mpcenc \
+  /usr/bin/oggenc \
+  /usr/bin/opusenc \
+  /usr/bin/wavpack \
+  /usr/lib/libcdda*.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
